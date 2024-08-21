bacb569d5e83fd0fad07e75b383dcac4b8abefa0
ShahwaizZahid <shahwaizmughal940@gmail.com>
2024-08-21 12:53:43 +0500

    add: restaurant details top image

--- lib/homePage/RestaurantDetails.dart
+++ lib/homePage/RestaurantDetails.dart
@@ -10,10 +10,11 @@ class RestaurantDetails extends StatelessWidget{
     return Scaffold(
       backgroundColor: Colors.white,
       bottomNavigationBar: BottomAppBar(
+        elevation: 0,
         child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          height: 50,
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          height: 70,
           decoration: BoxDecoration(
             color: kPrimaryColor,
             borderRadius: BorderRadius.circular(20),
@@ -26,10 +27,91 @@ class RestaurantDetails extends StatelessWidget{
             ],
           ),
           child: Row(
-            children: [],
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Order Now",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ],
           ),
         ),
       ),
+      body: MainScreen(index: index),
+    );
+  }
+}
+
+
+class MainScreen extends StatelessWidget{
+  final int index;
+  const MainScreen({super.key, required this.index});
+  @override
+  Widget build(BuildContext context) {
+    var he = MediaQuery.of(context).size;
+return Container(
+  padding: const EdgeInsets.only(top: 50),
+  height: he.height,
+  width: he.width,
+  decoration: BoxDecoration(
+    gradient: LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [
+        const Color.fromARGB(255, 83, 69, 164),
+        const Color.fromARGB(255, 66, 53, 165).withOpacity(.8),
+        const Color.fromARGB(255, 75, 53, 165).withOpacity(.6),
+        const Color.fromARGB(255, 121, 112, 159).withOpacity(.4),
+        const Color.fromARGB(255, 70, 53, 165).withOpacity(.2),
+        const Color(0xFF6F35A5).withOpacity(.1),
+        const Color(0xFF6F35A5).withOpacity(.05),
+        const Color(0xFF6F35A5).withOpacity(.025),
+      ],
+    ),
+  ),
+  child: SingleChildScrollView(
+    child: Column(
+      children: [
+TopImage(index: index),
+      ],
+    ),
+  ),
+);
+  }
+
+}
+
+
+class TopImage extends StatefulWidget{
+  final int index;
+  const TopImage({super.key, required this.index});
+
+  @override
+  State<TopImage> createState() => _TopImageState();
+}
+
+class _TopImageState extends State<TopImage> {
+  @override
+  Widget build(BuildContext context) {
+    var he = MediaQuery.of(context).size;
+    return Container(
+        height: he.height * 0.3,
+        width: he.width,
+        decoration: BoxDecoration(
+
+        color: Colors.amber,
+        borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(50),
+    bottomRight: Radius.circular(50),
+    ),
+    image: DecorationImage(
+    image: NetworkImage(restaurantList[widget.index].imageUrl),
+    fit: BoxFit.cover),
+    ),
     );
   }
 }
